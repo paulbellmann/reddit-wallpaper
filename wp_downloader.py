1f120dd89ea2f88e71fb18f17569626d8854191a
paulbellmann <pb>
2016-10-29 00:36:41 +0200

    first upload

---
+++ wp_downloader.py
@@ -0,0 +1,20 @@
+# Gets current top post
+# by paulbellmann
+
+import praw
+import urllib
+import os.path
+
+r = praw.Reddit(user_agent='wallpaper downloader /u/pwneed')
+
+def getImage():
+	submissions = r.get_subreddit('wallpaper').get_top_from_day(limit=1)
+	submission = submissions.next()
+	# set path and file name here
+	myPath = "/home/pi/Pictures/Wallpaper/wp_" + str(submission.id) + ".jpg"
+
+	if not os.path.isfile(myPath):
+		urllib.urlretrieve(submission.url, myPath)
+		print submission.title + " saved!"
+
+getImage()
