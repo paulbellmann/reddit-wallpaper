5e6f47221fe932a01ea17ac0d38ee479294799ac
paulbellmann <paul.bellmann@yahoo.de>
2016-10-31 11:04:21 +0100

    function edits
    = new parameter

--- wp_downloader.py
+++ wp_downloader.py
@@ -7,12 +7,15 @@ import os.path
 
 r = praw.Reddit(user_agent='wallpaper downloader /u/user')
 
-def getImage():
-	submissions = r.get_subreddit('wallpaper').get_top_from_day(limit=1)
+def getImage(subreddit):
+	submissions = r.get_subreddit(subreddit).get_top_from_day(limit=1)
 	submission = submissions.next()
 	# set path and file name here
-	myPath = "/home/pi/Pictures/Wallpaper/wp_" + str(submission.id) + ".jpg"
+	myPath = "/Users/user/Pictures/wp_" + str(submission.id) + ".jpg"
 
 	if not os.path.isfile(myPath):
+		urllib.urlretrieve(submission.url, myPath)
+		print submission.title + " saved."
 
-getImage()
+getImage('EarthPorn')
+getImage('Wallpaper')
